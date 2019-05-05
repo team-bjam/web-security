8561e1e3ac41d02109db8824b0e189255c163396
andrinja <andra.zeberga@gmail.com>
2019-05-05 22:08:49 +0200

    - Fixed Failed login attempt counter
    - Removed example routes
    - Removed example controller

--- app/routes.php
+++ app/routes.php
@@ -14,11 +14,4 @@ $router->post('login', 'LoginController@store');
 /**
  * Authenticated user routes
  */
-$router->get('dashboard', 'DashboardController@index');
 
-$router->get('', 'PagesController@home');
-$router->get('about', 'PagesController@about');
-$router->get('contact', 'PagesController@contact');
-
-$router->get('users', 'UsersController@index');
-$router->post('users', 'UsersController@store');
